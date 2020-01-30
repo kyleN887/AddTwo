fe44ed86ec678579e277b0ad1a09e4e98ac2ebca
kyleN887 <kyle887@my.yorku.ca>
2020-01-29 22:36:17 -0500

    adding another change

--- Solution.java
+++ Solution.java
@@ -60,7 +60,7 @@ public class Solution {
 		
 		Solution mine = new Solution("Kyle"); 
 		ListNode answer = mine.addTwoNumbers(dope, kyle); 
-		System.out.println("damn hit change");
+		System.out.println("damn hit another change");
 				
 		  while(answer != null) {
 			System.out.println(answer.val); 
